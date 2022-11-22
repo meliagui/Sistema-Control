49eb1a2934b26ee82c59d1044a28faf4284fd6bf
meliagui <melissaguil1121@yahoo.com>
2022-11-22 18:29:24 -0500

    sin encriptacion

--- controlador/login.php
+++ controlador/login.php
@@ -4,7 +4,7 @@
 if (!empty($_POST["btningresar"])) {
     if (!empty($_POST["usuario"]) and !empty($_POST["usuario"]) ) {
     $usuario=$_POST["usuario"];
-    $password=md5($_POST["password"]);
+    $password=($_POST["password"]);
     $sql=$conexion->query("select * from usuario where usuario='$usuario' and password='$password'");
     if ($datos=$sql->fetch_object()) {
        $_SESSION["nombre"]=$datos->nombre;
