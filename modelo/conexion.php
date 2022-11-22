6b49d915765d66d2c01846a54535f90278acea15
meliagui <melissaguil1121@yahoo.com>
2022-11-22 17:22:22 -0500

    add  conexion Railway

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost","root","M123456789","dbcontrol","3306");
+$conexion = new mysqli("containers-us-west-29.railway.app","root","8SLTyz4xxlMg6754J2CZ","railway","6015");
 $conexion-> set_charset("utf8");
 date_default_timezone_set("America/Lima");
 ?>
