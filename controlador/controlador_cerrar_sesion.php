20b018b67716d3b1811d688ecf059fe8eec7b180
meliagui <melissaguil1121@yahoo.com>
2022-11-25 08:57:25 -0500

    reverse fix

--- controlador/controlador_cerrar_sesion.php
+++ controlador/controlador_cerrar_sesion.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
 session_destroy();
-header("location:/https://sistema-control-production.up.railway.app/vista/login/login.php");
-//header("location:/Sistema-Control/vista/login/login.php");
+header("location:/Sistema-Control/vista/login/login.php");
 ?>
